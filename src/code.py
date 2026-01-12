f122f6b0f0335b46d521a6a01725813fc1323187
rshea33 <rshea781@gmail.com>
2026-01-11 22:38:20 -0500

    Add alternating train group

--- src/code.py
+++ src/code.py
@@ -8,13 +8,20 @@ from metro_api import MetroApi, MetroApiOnFireException
 STATION_CODE = config['metro_station_code']
 TRAIN_GROUP = config['train_group']
 REFRESH_INTERVAL = config['refresh_interval']
+ALTERNATING_TRAINS = TRAIN_GROUP == '0'
+CURRENT_TRAIN_GROUP = '1'
 
 def refresh_trains() -> [dict]:
-	try:
-		return MetroApi.fetch_train_predictions(STATION_CODE, TRAIN_GROUP)
-	except MetroApiOnFireException:
-		print('WMATA Api is currently on fire. Trying again later ...')
-		return None
+    global CURRENT_TRAIN_GROUP
+    group = CURRENT_TRAIN_GROUP if ALTERNATING_TRAINS else TRAIN_GROUP
+    try:
+        trains = MetroApi.fetch_train_predictions(STATION_CODE, group)
+    except MetroApiOnFireException:
+        print('WMATA Api is currently on fire. Trying again later ...')
+        return None
+    if ALTERNATING_TRAINS:
+        CURRENT_TRAIN_GROUP = '2' if CURRENT_TRAIN_GROUP == '1' else '1'
+    return trains
 
 train_board = TrainBoard(refresh_trains)
 
